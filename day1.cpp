9f3c2a55228978e77ac9ecc4e5549170e46633a3
Amna Shokat <153503548+AmnaShokat@users.noreply.github.com>
2025-05-07 10:08:50 +0500

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,32 @@
+// day 01
+
+
+// c++ is high level programming language 
+// game development and computer programming
+// extention of C.both have same syntax
+
+#include<iostream>
+// a header file library which add functionality in programming
+using namespace std;
+int main(){
+    // print string in ""
+    cout<<"hello there i am amna ,a software engineer";
+    
+    // print string
+    cout<<3;
+    
+    // comment
+    // thing that wrote in front of double forward slash 
+    // is ignored by a comiler
+    
+    // new line
+    cout<<endl;
+    cout<<"\n";
+    
+    
+    
+    
+    
+    
+    
+}
